ab435dc2bb9a7c0d2bc2283a381e4260de565c80
banduo <139312175@qq.com>
2018-06-14 16:55:50 +0800

    222

--- src/main/java/com/catherine/DemoApplication.java
+++ src/main/java/com/catherine/DemoApplication.java
@@ -10,6 +10,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("11111");
+		System.out.println("22");
 	}
 }
